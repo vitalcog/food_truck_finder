95f0d11acc15c125952879034fd38a626ff2a147
Ken Coppola <mwave317@gmail.com>
2017-10-10 14:00:38 -0400

    Adding the store files

--- src/components/map_box.js
+++ src/components/map_box.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
 class MapBox extends Component {
+
+  componentWillMount() {
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY')
+    .then(response => response.json)
+    .then(console.log(response));
+  }
+
   render() {
     return (
       <div className="mapbox">
-        <div className="googleMap"></div>
+        <div className="googleMap">
+        <Map/>
+        </div>
         <div className="sliderGuage">
           dynamic slide for adjusting search radius goes here
         </div>
+       
       </div>
     );
-  }
-}
+  };
+};
+
+
 
 export default MapBox;
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'react-redux';
+
+function reducer (state, action ) {
+
+};
+
+const store = createStore(reducer, {
+
+});
