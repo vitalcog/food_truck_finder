bd2ef9fc666039aca4399cf395afef4ef5c28003
Ken Coppola <mwave317@gmail.com>
2017-10-13 11:29:11 -0400

    Uploading the Files with the Map as a thing

--- src/components/map_box.js
+++ src/components/map_box.js
@@ -5,19 +5,19 @@ class MapBox extends Component {
     window.mapboxgl.accessToken = 'pk.eyJ1IjoiY2p6ZWxlZG9uIiwiYSI6ImNqOG5jdnlhODE5a3MycW11MWo1eGV2Y2QifQ.WZStz_i8Bt1B4OEZJMg_WA';
     
     new window.mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9',
-      zoom: 13,
-    });
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+      center: [-80.8464, 35.2269], // starting position [lng, lat]
+      zoom: 14 // starting zoom
+  });
   }
 
   render() {
     return (
       <div className="mapbox">
+        
         <div id="map"></div>
-        <div className="sliderGuage">
-          dynamic slide for adjusting search radius goes here
-        </div>
+
       </div>
     );
   };
