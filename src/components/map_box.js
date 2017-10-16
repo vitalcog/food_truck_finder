cc4dec04f7aa9de694a2232010f897b5ce6694d8
Ken Coppola <mwave317@gmail.com>
2017-10-16 15:02:00 -0400

    Uploading Files with the Points working with an external file.

--- src/components/map_box.js
+++ src/components/map_box.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import coordinates from '../coordinates.js';
 
 
 class MapBox extends Component {
@@ -6,25 +7,42 @@ class MapBox extends Component {
   componentDidMount() {
     window.mapboxgl.accessToken = 'pk.eyJ1IjoiY2p6ZWxlZG9uIiwiYSI6ImNqOG5jdnlhODE5a3MycW11MWo1eGV2Y2QifQ.WZStz_i8Bt1B4OEZJMg_WA';
     
-    new window.mapboxgl.Map({
+    const map = new window.mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
       center: [-80.8464, 35.2269], // starting position [lng, lat]
       zoom: 14 // starting zoom
   });
-  // window.mapboxgl.addLayer({
-  //   id: 'points',
-  //   source: 'mapbox-streets',
-  //   "source-layer": 'points',
-  //   type: 'circle',
-  // });
 
-  // window.mapboxgl.addSourc('https://desolate-lowlands-68945.herokuapp.com/foodtrucks/mi-barrios-halal-cart-charlotte', {
-  //   type: geojson,
-  //   data: 'https://desolate-lowlands-68945.herokuapp.com/foodtrucks/mi-barrios-halal-cart-charlotte',
-  // })
+  const locations = coordinates.map(truck => { return {'type': 'Feature', 'properties': {},'geometry': { 'type': 'Point','coordinates': truck,}
+  }});
+
+  var watchID = navigator.geolocation.watchPosition(function(position) {
+    findLocation(position.coords.latitude, position.coords.longitude);
+  });
+navigator.geolocation.clearWatch(watchID);
+
+console.log(locations);
+map.on('load', () => {
+  map.addSource('pointsSource', {
+    type: 'geojson',
+    data: {
+      'type': 'FeatureCollection',
+      'features': locations,
+      
+    }
+   });
+
+   map.addLayer({
+    id: 'points',
+    source: 'pointsSource',
+    type: 'circle',
+  });
+
+   });
   }
 
+  
   render() {
     return (
       <div className="mapbox">
---
+++ src/coordinates.js
@@ -0,0 +1,8 @@
+let coordinates =  [
+          [-80.8470, 35.2269],
+          [-80.8464, 35.2269],
+          [-80.8473, 35.2264],
+        ];
+
+export default coordinates;
+        
