72dbd899464101a5f28665a14d12291447d14d3b
Ken Coppola <mwave317@gmail.com>
2017-10-16 17:28:42 -0400

    Uploading Files with the current Location working on the map.

--- src/components/map_box.js
+++ src/components/map_box.js
@@ -6,7 +6,7 @@ class MapBox extends Component {
 
   componentDidMount() {
     window.mapboxgl.accessToken = 'pk.eyJ1IjoiY2p6ZWxlZG9uIiwiYSI6ImNqOG5jdnlhODE5a3MycW11MWo1eGV2Y2QifQ.WZStz_i8Bt1B4OEZJMg_WA';
-    
+    //Adds the map
     const map = new window.mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
@@ -14,15 +14,42 @@ class MapBox extends Component {
       zoom: 14 // starting zoom
   });
 
-  const locations = coordinates.map(truck => { return {'type': 'Feature', 'properties': {},'geometry': { 'type': 'Point','coordinates': truck,}
+  //Loops over the coordinates to place the locations on the map.
+  const locations = coordinates.map(truck => { return {'type': 'Feature',
+   'properties': {},'geometry': { 'type': 'Point','coordinates': truck,}
   }});
 
-  var watchID = navigator.geolocation.watchPosition(function(position) {
-    findLocation(position.coords.latitude, position.coords.longitude);
-  });
-navigator.geolocation.clearWatch(watchID);
+//   let findLocation = function (position) {
+//   let location = [position.coords.longitude, position.coords.latitude];
+// console.log(location);
+//    // Put the user marker on the map
+//   map.addSource('findLocations', {
+//     type: 'geojson',
+//     data: {
+//       'type': 'FeatureCollection',
+//       'features': [
+//         {'type': 'Feature',
+//         'properties': {},'geometry': { 'type': 'Point','coordinates': location} }
+//       ],
+      
+//     }
+//    });
 
-console.log(locations);
+//    map.addLayer({
+//     id: 'current',
+//     source: 'findLocations',
+//     type: 'circle',
+//   });
+//  }
+  
+  // function noLocation (positionError) {
+  //   console.log(positionError);
+  // }
+
+  //Gets the users position
+  // let watchID = navigator.geolocation.watchPosition(findLocation, noLocation);
+  // let clearID = navigator.geolocation.clearWatch(watchID);
+
 map.on('load', () => {
   map.addSource('pointsSource', {
     type: 'geojson',
@@ -39,10 +66,16 @@ map.on('load', () => {
     type: 'circle',
   });
 
-   });
-  }
+  map.addControl(new window.mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
+map.addControl(new window.mapboxgl.NavigationControl());
+});
 
-  
+}
   render() {
     return (
       <div className="mapbox">
