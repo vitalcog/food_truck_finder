8315ff73325e9c54b62ebc14ac3a0c935e558813
Ken Coppola <mwave317@gmail.com>
2017-10-19 16:07:17 -0400

    Uplading files with the current position being generated

--- src/components/map_box.js
+++ src/components/map_box.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class MapBox extends Component {
-  // google api key = AIzaSyCniENfrb5Jtl90ZCVLMfsyYGqUxWxLEas
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+    }
+  }
+  
   componentDidMount() {
 
     let coordinates;
@@ -9,7 +16,8 @@ class MapBox extends Component {
     .then(response => response.json())
     .then(response => { coordinates = response.businesses.map(response =>
      { return {'type': 'Feature',
-     'properties': {},'geometry': { 'type': 'Point','coordinates':  [response.coordinates.longitude, response.coordinates.latitude]}}})});
+     'properties': { },'geometry': { 'type': 'Point','coordinates':  [response.coordinates.longitude, response.coordinates.latitude]}}})});
+
 
     window.mapboxgl.accessToken = 'pk.eyJ1IjoiY2p6ZWxlZG9uIiwiYSI6ImNqOG5jdnlhODE5a3MycW11MWo1eGV2Y2QifQ.WZStz_i8Bt1B4OEZJMg_WA';
     //Adds the map
@@ -22,6 +30,8 @@ class MapBox extends Component {
 
   //Retreives the json of foodtrucks and returns the coordinates to the map in geojson. 
 
+ 
+
 
 map.on('load', () => {
   map.addSource('pointsSource', {
@@ -29,36 +39,95 @@ map.on('load', () => {
     data: {
       'type': 'FeatureCollection',
       'features': coordinates,
-      
     }
    });
-
    map.addLayer({
     id: 'points',
     source: 'pointsSource',
     type: 'circle',
   });
-
-  map.addControl(new window.mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true
-}));
-map.addControl(new window.mapboxgl.NavigationControl());
 });
 
+  let watchID = navigator.geolocation.watchPosition(function(position) {
+    bigBrother(position);
+  });
+ 
+  const bigBrother= (position)=> {
+    
+   this.setState({
+     latitude: position.coords.latitude, 
+     longitude : position.coords.longitude
+    }, () => {
+
+      map.addSource('movingAlong', {
+        type: 'geojson',
+        data: {
+          'type': 'FeatureCollection',
+          'features': [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [
+                this.state.longitude,
+                this.state.latitude,
+              ],
+            }
+          }]
+        }
+       });
+
+       map.addLayer({
+        id: 'currentLocation',
+        source: 'movingAlong',
+        type: 'circle',
+      });
+       console.log(this.state.longitude,
+        this.state.latitude);
+    
+
+  });
+};
+
+
+//   map.addSource('movingAlong', {
+//     type: 'geojson',
+//     data: {
+//       'type': 'FeatureCollection',
+//       'features': [{
+//         type: 'Feature',
+//         geometry: {
+//           type: 'Point',
+//           coordinates: apple,
+//           // coordinates: [-80.8464, 35.2272],
+//         }
+//       }]
+//     }
+// Shows the users current location on the map
+//   map.addControl(new window.mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         enableHighAccuracy: true
+//     },
+//     trackUserLocation: true
+// }));
+map.addControl(new window.mapboxgl.NavigationControl());
+
+
+//Centers the point the user selected on the map 
 map.on('click', 'points', function (e) {
   map.flyTo({center: e.features[0].geometry.coordinates});
 });
 
+
+
+
 }
   render() {
+  console.log(this.state.longitude, this.state.latitude);
     return (
       <div className="mapbox">
+        <div id="map">
         
-        <div id="map"></div>
-
+        </div>
       </div>
     );
   };
