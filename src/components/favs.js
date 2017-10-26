24ecd10748f859c748cfc8e70799406c45f3f749
Chad Windham <chadwindham@Chads-MacBook-Pro.local>
2017-10-26 17:33:09 -0400

    prepared changes for displaying directions beneath map

--- src/components/favs.js
+++ src/components/favs.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
-import TopFive from './top_five';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react'
+import TopFive from './top_five'
+import {Link} from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Favs extends Component {
 
   render() {
-    return (
-      <div className="favs">
-        <div>
-          local trucks
+    if (this.props.instructions.length === 0) {
+      return (
+        <div className="favs">
+          <div>
+            local trucks
+          </div>
+          <TopFive/>
+          <Link className="link" to='/allFavs'>All</Link>
         </div>
-        <TopFive/>
-        <Link className="link" to='/allFavs'>All</Link>
-      </div>
-    );
+      )
+    } else {
+      return (
+        <div className="favs"></div>
+      )
+    }
   }
 }
 
-export default Favs;
+// connecting to Redux store below this point
+
+function state2props(state){
+  return {
+    favorites: state.favorites,
+    instructions: state.instructions
+  }
+}
+
+function dispatch2props(dispatch) {
+  return {}
+}
+
+export default connect (state2props, dispatch2props ) (Favs)
